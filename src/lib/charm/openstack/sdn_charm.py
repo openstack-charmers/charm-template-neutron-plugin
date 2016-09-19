e1aeeb28c7cea77b6f501da6dce88e0a3b6493fc
Liam Young <liam.young@canonical.com>
2016-09-19 11:33:04 +0000

    First cut of Template for Neutron Plugin charms

---
+++ src/lib/charm/openstack/sdn_charm.py
@@ -0,0 +1,20 @@
+import socket
+import subprocess
+
+import charmhelpers.core.hookenv as hookenv
+import charmhelpers.contrib.network.ip as ch_ip
+import charms_openstack.charm
+# import charms_openstack.sdn.odl as odl
+# import charms_openstack.sdn.ovs as ovs
+
+
+class SDNCharm(charms_openstack.charm.OpenStackCharm):
+
+    # Internal name of charm
+    service_name = name = 'sdn'
+
+    # First release supported
+    release = 'liberty'
+
+    # List of packages to install for this charm
+    packages = []
---
+++ src/reactive/sdn_charm_handlers.py
@@ -0,0 +1,21 @@
+# Copyright 2016 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import charms_openstack.charm as charm
+import charms.reactive as reactive
+
+# This charm's library contains all of the handler code associated with
+# sdn_charm
+import charm.openstack.sdn_charm as sdn_charm  # noqa
+
